23d6e9625309a44a17167286f9f2855a5990b5ed
Hanz <2409281700@qq.com>
2024-12-03 09:38:49 +0800

    更新图标 网页名

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,13 +3,13 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: '宁波轨道交通全网线路示意图',
+    title: '北京地铁线路示意图',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '宁波轨道交通地图' }
+      { hid: 'description', name: 'description', content: '北京地铁线路示意图' }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/bj.ico' }]
   },
   css: ['~styles/cnuc/index.styl'],
   plugins: ['~plugins/element-ui'],
